6b782351cddd1b080d7c4cc31065186f32ff4b28
Remilia <HinanawiCirno@outlook.com>
2016-08-23 10:32:45 +0800

    Init  Athena-DesignPattern

---
+++ Athena-Java/src/main/java/me/mushen/athena/java/classes/SpaceShipControls.java
@@ -0,0 +1,16 @@
+package me.mushen.athena.java.classes;
+
+/**
+ * @Desc
+ * @Author Remilia
+ * @Create 2016-08-22
+ */
+public class SpaceShipControls {
+    public void up(int volocity){}
+    public void down(int volocity){}
+    public void left(int volocity){}
+    public void right(int volocity){}
+    public void forward(int volocity){}
+    public void back(int volocity){}
+    public void turboBoost(){}
+}
